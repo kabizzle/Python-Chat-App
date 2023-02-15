cc0ae17e1959c3a7a6a17c5ed9c35f2cd1003489
Kabir Bissessar <kabirrbissessar@gmail.com>
2023-02-16 00:57:36 +0200

    fixed group chats, implemented ipv6, users can receive messages offline

--- client.py
+++ client.py
@@ -1,25 +1,31 @@
 import socket
 import threading
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 5678
-SERVER_CONFIG = (HOST, PORT)
+# HOST = socket.gethostbyname(socket.gethostname())
+# HOST = '0.0.0.0'
+# PORT = 5678
+# SERVER_CONFIG = (HOST, PORT)
+HOST_INFO = socket.getaddrinfo('localhost', 8080)
+SERVER_CONFIG_IPv4 = HOST_INFO[1][4]
+SERVER_CONFIG_IPv6 = HOST_INFO[0][4]
 FORMAT = 'utf-8'
 MSG_LENGTH = 2048
 DIRECT_MSG = '/direct'
 GROUP_MSG = '/group'
 DISCONNECT_MSG = '/leave'
 WELCOME_MSG = f"Welcome to the server. You can send a message to everyone by just typing :)\nTo send a direct message to another user, send {DIRECT_MSG}. \nTo create a group chat, send {GROUP_MSG}\n"
+CONNECTED = False
 
 # Function to listen for messages from server
-def listen_to_server(client):
+def listen_to_server(client, CONNECTED):
 
-    connected = True
-    while connected:
+    # connected = True
+    while CONNECTED:
         message = client.recv(MSG_LENGTH).decode(FORMAT)
 
         if message == DISCONNECT_MSG:
-            connected = False
+            # connected = False
+            CONNECTED = False
         elif message != '':
             # username = message.split(":")[0]
             # content = message.split(":")[1]
@@ -29,7 +35,7 @@ def listen_to_server(client):
 
 
 # Function to send communicate client info with server
-def initialise_on_server(client):
+def initialise_on_server(client, CONNECTED):
 
     username = input("Please enter your username: ")
 
@@ -42,36 +48,52 @@ def initialise_on_server(client):
     # else:
         # print("Username cannot be empty.")
     
-    client.sendall(username.encode(FORMAT))
+    client.send(username.encode(FORMAT))
+    CONNECTED = True
          
-    threading.Thread(target=listen_to_server, args=(client, )).start()
-    send_message_to_server(client)
+    threading.Thread(target=listen_to_server, args=(client, CONNECTED)).start()
+    send_message_to_server(client, CONNECTED)
 
 
 # Function to send message to server
-def send_message_to_server(client):
-    while True:
+def send_message_to_server(client, CONNECTED):
+
+    while CONNECTED:
         message = input()    
 
         if message != '':
-            client.sendall(message.encode(FORMAT))
+            client.send(message.encode(FORMAT))
 
 
 # Main function
 def start():
 
     # Creating client socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip_type = input(f'[INITIALIZATION] What type of connection would you like to establish? ipv4/ipv6\n')
 
-    # Attempt to connect to server
-    try:
-        client.connect(SERVER_CONFIG)
-        print(f"Client successfully connected to server {HOST} {PORT}")
-        print(WELCOME_MSG)
-    except:
-        print(f"Client unable to connect to server {HOST} {PORT}")
+    if ip_type.lower() == 'ipv6':
+        client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        client.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
 
-    initialise_on_server(client)
+        # Attempt to connect to server
+        try:
+            client.connect(SERVER_CONFIG_IPv6)
+            print(f"Client successfully connected to server {SERVER_CONFIG_IPv6} over {ip_type}")
+            print(WELCOME_MSG)
+        except:
+            print(f"Client unable to connect to server {SERVER_CONFIG_IPv6}")
+    else:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Attempt to connect to server
+        try:
+            client.connect(SERVER_CONFIG_IPv4)
+            print(f"Client successfully connected to server {SERVER_CONFIG_IPv4} over {ip_type}")
+            print(WELCOME_MSG)
+        except:
+            print(f"Client unable to connect to server {SERVER_CONFIG_IPv4}")
+
+    initialise_on_server(client, CONNECTED)
 
 # if __name__ == "__main__":
 #     main()
--- server.py
+++ server.py
@@ -4,17 +4,21 @@ from queue import Queue
 from datetime import datetime
 
 # TODO:
-# - identify direct vs groupchat messages
-#       copy string format for broadcast message and add 'from [user]' or 'from [group]'
 # - the group chat admin can add/remove members, edit group info and delete group 
-# - check if gp members in server
-# - handle leaving server in client - exit to terminal
-# - implement message queue to save messages for offline users
-# - implement choosing whether to use IPv4 or IPv6
+# - check if inputted users in server when creating group
+# - add 'last online' to user object
+# - inform senders if sender is offline and when they were last online
+# - implement read receipts
+# - show when users leave the server
 # - implement file transfer
-# - implement gui
+# ? implement gui
 # ? implement parsing message into desired format: [sender]~[receiver]~[content]~[timestamp]~[sent]~[received]
-
+#
+#
+# KINDA FIXED:
+# - handle leaving server in client - exit to terminal
+#
+#
 # DONE:
 # - when users send dm command, it will only message desired client
 # - a user can send group chat command to form a group with clients A and B etc.
@@ -24,10 +28,19 @@ from datetime import datetime
 # - let users know when added to a group chat
 # - make creating group chat a command in group mode rather than default
 # - users can send messages to group even if they didnt create it
+# - implement message queue to save messages for offline users
+# - identify direct vs groupchat messages
+#       copy string format for broadcast message and add 'from [user]' or 'from [group]'
+# - implement choosing whether to use IPv4 or IPv6
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 5678
-SERVER_CONFIG = (HOST, PORT)
+# HOST = socket.gethostbyname(socket.gethostname())
+# HOST = '0.0.0.0'
+HOST = ""
+PORT = 8080
+SERVER_CONFIG_IPv4 = (HOST, PORT)
+# HOST_INFO = socket.getaddrinfo('localhost', 5678)
+# SERVER_CONFIG_IPv4 = HOST_INFO[1][4]
+# SERVER_CONFIG_IPv6 = HOST_INFO[0][4]
 FORMAT = 'utf-8'
 MSG_LENGTH = 2048
 DIRECT_MSG = '/direct'
@@ -35,7 +48,7 @@ GROUP_MSG = '/group'
 DISCONNECT_MSG = '/leave'
 
 # Template for storing client data in dictionary
-user = lambda username, socket, online : {"username": username, "client": socket, "online": online}
+user = lambda username, socket, online : {"username": username, "client": socket, "online": online, "queue": Queue()}
 
 # list of clients connected to server
 users = {} 
@@ -46,19 +59,20 @@ groups = {}
 # active_clients = []
 
 # Queue to store messages while user offline
-message_queue = Queue()
+# message_queue = Queue()
 
 # Creating server socket object
 # AF_INET - IPv4 addresses will be used
 # SOCK_STREAM - TCP packets will be used
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# ip_type = input(f'[INITIALIZATION] What type of connection would you like to establish? ipv4/ipv6')
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Attempts to create server at given HOST IP and PORT
-try:
-    server.bind(SERVER_CONFIG)
-    print(f"[RUNNING] Server is running at {HOST} {PORT}")
-except:
-    print(f"[ERROR] Server unable to be created at host {HOST} and port {PORT}")
+# # Attempts to create server at given HOST IP and PORT
+# try:
+#     server.bind(SERVER_CONFIG)
+#     print(f"[RUNNING] Server is running at {HOST} {PORT}")
+# except:
+#     print(f"[ERROR] Server unable to be created at host {HOST} and port {PORT}")
 
 # Function to handle interaction of a client
 # def client_handler(client):
@@ -93,25 +107,31 @@ except:
 
 # Function to send message to a single client
 def send_to_client(client, message):
-    client.sendall(message.encode(FORMAT))
+    client.send(message.encode(FORMAT))
 
 
 # Function to send messages to all active clients
 def broadcast(message):
     for client in users.keys():
-        send_to_client(users[client]["client"], message)
+        if users[client]["online"]:
+            send_to_client(users[client]["client"], message)
+        else:
+            users[client]["queue"].put(message)
 
 def direct(username, message):
     if username in users.keys():
         if users[username]["online"]:
             send_to_client(users[username]["client"], message)
         else:
-            message_queue.put(message)
+            users[username]["queue"].put(message)
 
 def group(group_name, message):
     group_users = groups[group_name]["users"]
     for member in group_users:
-        send_to_client(users[member]["client"], message)
+        if users[member]["online"]:
+            send_to_client(users[member]["client"], message)
+        else:
+            users[member]["queue"].put(message)
 
 
 # # Function to check if user exists in server
@@ -138,16 +158,19 @@ def client_handler(client, address):
     connected = True
     
     username = client.recv(MSG_LENGTH).decode(FORMAT)
-    # if username in users:
-    #     users[username]["client"] = client
-    #     users[username]["online"] = True
-    # else:
-    users[username] = user(username, client, True)
+    if username in users.keys():
+        users[username]["client"] = client
+        users[username]["online"] = True
+    else:
+        users[username] = user(username, client, True)
     # users.append((username, client))
         
     while connected:
+        while not users[username]["queue"].empty():
+            send_to_client(client, f'{users[username]["queue"].get()} \n')
         message = client.recv(MSG_LENGTH).decode(FORMAT)
-        time = datetime.now()
+        current_time = datetime.now()
+        time = current_time.strftime("%d/%m/%y %H:%M:%S")
 
         if message == DISCONNECT_MSG:
             send_to_client(client, "/leave")
@@ -160,7 +183,8 @@ def client_handler(client, address):
                 new_msg = client.recv(MSG_LENGTH).decode(FORMAT)
                 if '~' in new_msg:
                     parsed_msg = new_msg.split('~')
-                    direct(parsed_msg[0], parsed_msg[1])
+                    direct_msg = f'[DIRECT][{time}] {users[username]["username"]} sent you: {parsed_msg[1]}'
+                    direct(parsed_msg[0], direct_msg)
                 elif new_msg == 'exit':
                     direct_mode = False
                 else:
@@ -182,7 +206,7 @@ def client_handler(client, address):
                 # check if user is part of group before creating
                 create_group(group_name, parsed_group_users, username)
                 # groups[group_name] = {"users": parsed_group_users, "admin": username}
-                group(group_name, f"You have been added to the group chat{group_name}!")
+                group(group_name, f"You have been added to the group chat {group_name}!")
 
             send_to_client(client, f"You are a member of the following group chats: {user_groups}\nSend a message of the form '[group name]~[message]' to send a message to that group chat.")
 
@@ -193,7 +217,8 @@ def client_handler(client, address):
                 if '~' in new_msg:
                     # current_group = new_msg[1:]
                     parsed_msg = new_msg.split('~')
-                    group(parsed_msg[0], parsed_msg[1])
+                    group_msg = f'[GROUP][{time}] {users[username]["username"]} sent to group {parsed_msg[0]}: {parsed_msg[1]}'
+                    group(parsed_msg[0], group_msg)
                     
                 elif new_msg == 'exit':
                     group_mode = False
@@ -201,7 +226,7 @@ def client_handler(client, address):
                     send_to_client(client, "Incorrect format, direct message not sent")
     
         else:
-            broadcast(f'[{time.strftime("%d/%m/%y %H:%M:%S")}] {users[username]["username"]}: {message}')
+            broadcast(f'[GENERAL][{time}] {users[username]["username"]}: {message}')
 
         print(f"{address}: {message}")
     
@@ -227,8 +252,42 @@ def start():
     #     print(f"Server is running at {HOST} {PORT}")
     # except:
     #     print(f"Server unable to be created at host {HOST} and port {PORT}")
-    
-    print("[LISTENING] Server is listening on {HOST} {PORT}")
+    # ip_type = input(f'[INITIALIZATION] What type of connection would you like to establish? ipv4/ipv6\n')
+
+    # if ip_type.lower() == 'ipv6':
+    #     server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    #     server.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+    #     # server.connect(SERVER_CONFIG_IPv6)
+    #     try:
+    #         server.bind(SERVER_CONFIG_IPv6)
+    #         print(f"[RUNNING] Server is running at {SERVER_CONFIG_IPv6} over {ip_type}")
+    #     except:
+    #         print(f"[ERROR] Server unable to be created at host {SERVER_CONFIG_IPv6}")
+
+    #     print(f"[LISTENING] Server is listening on {SERVER_CONFIG_IPv6}")
+    # else:
+    #     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Attempts to create server at given HOST IP and PORT
+        # try:
+        #     server.bind(SERVER_CONFIG_IPv4)
+        #     print(f"[RUNNING] Server is running at {SERVER_CONFIG_IPv4} over {ip_type}")
+        # except:
+        #     print(f"[ERROR] Server unable to be created at host {SERVER_CONFIG_IPv4}")
+
+        # print(f"[LISTENING] Server is listening on {SERVER_CONFIG_IPv4}")
+        # # Listen for clients
+
+    # Attempts to create server at given HOST IP and PORT
+    server = socket.create_server(SERVER_CONFIG_IPv4, family=socket.AF_INET6, dualstack_ipv6=True)
+
+    try:
+        # server.bind(SERVER_CONFIG_IPv4)
+        print(f"[RUNNING] Server is running at {SERVER_CONFIG_IPv4} and accepts both IPv4 and IPv6 connections")
+    except:
+        print(f"[ERROR] Server unable to be created at host {SERVER_CONFIG_IPv4}")
+
+    print(f"[LISTENING] Server is listening on {SERVER_CONFIG_IPv4}")
     # Listen for clients
     server.listen()
  
